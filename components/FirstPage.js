91f9e5c22df900fa8898ca100329cc1d449884b7
Osama Al Madhoun <o.almadhoun@unitone.co>
2022-12-04 11:41:53 +0200

    first init

---
+++ components/FirstPage.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import {Container , Col , Row} from "react-bootstrap";
+import styles from '../styles/First.module.css'
+import Image from "next/image";
+import TopLeftImage from '../public/images/page_1/Icons_1_21.png'
+import Icone2 from '../public/images/page_1/icons_2_new_c.png';
+import Icone3 from '../public/images/page_1/icons_1_03_new.png';
+import Icone4 from '../public/images/page_1/icons_1_04_new.png';
+
+function FirstPage(props) {
+    return (
+            <div className={styles.first_page_background}>
+                 <Container fluid={+true}>
+                     <Row className="justify-content-lg-start
+                     justify-content-xl-start
+                     justify-content-xxl-start
+                     align-items-start
+                      justify-content-center">
+                     <Col xs={12} md={4} lg={4}><Image src={TopLeftImage}
+                               className={styles.top_left_image}
+                               alt={"first imageg"}
+                               width={180}
+                               height={180}
+                   ></Image></Col>
+                     </Row>
+                     <Row>
+                         <Col>
+                             <div className={'d-flex flex-column align-items-center justify-content-center'}>
+                                 <h4 className={'my-2'}>马来西亚首创</h4>
+                                 <h1 className={'my-2'}>「捏骨式」</h1>
+                                 <h4 className={'my-2'}>半永久纹绣品牌</h4>
+                             </div>
+                         </Col>
+                     </Row>
+                     <Row className={'mt-md-3 mt-5'}>
+                         <Col>
+                             <div className={'d-flex flex-column flex-md-row justify-content-center align-items-stretch'}>
+                                 <div className={'px-4 py-2 border rounded mx-1 d-inline-flex align-items-center'} style={{backgroundColor:'#f9f0f0'}}>
+                                     <Image src={Icone2} alt={"icone 2"}
+                                            width={40} height={40}
+                                            className={'rounded-circle mx-2'}
+                                     ></Image>
+                                     自然伪妆感
+                                 </div>
+                                 <div className={'px-4 py-2 border  rounded mx-1 d-inline-flex align-items-center'} style={{backgroundColor:'#f9f0f0'}}>
+                                     <Image src={Icone3} alt={"icone 2"}
+                                            width={40} height={40}
+                                            className={'rounded-circle mx-2'}
+                                     ></Image> 科技修饰脸型
+                                 </div>
+                                 <div className={'px-4 py-2 border  rounded mx-1 d-inline-flex align-items-center'} style={{backgroundColor:'#f9f0f0'}}>
+                                     <Image src={Icone4} alt={"icone 2"}
+                                            width={40} height={40}
+                                            className={'rounded-circle mx-2'}
+                                     ></Image> 无疼过程
+                                 </div>
+                             </div>
+                         </Col>
+                     </Row>
+
+                 </Container>
+            </div>
+    );
+}
+
+export default FirstPage;
---
+++ components/SecondPage.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import styles from '../styles/Second.module.css';
+import Icon1 from '../public/images/page_2/Icons_1_05.png';
+import Icon2 from '../public/images/page_2/Icons_1_06.png';
+import Icon3 from '../public/images/page_2/Icons_1_07.png';
+import Image from "next/image";
+function SecondPage(props) {
+    return (
+        <>
+            <div className={styles.left_image_background}></div>
+            <div className={styles.right_image_background}></div>
+            <div className={styles.card_sense}>
+                    <h4 className={'text-center w-100'}>SENSE EMBROIDERY</h4>
+              <div className={'d-flex flex-column justify-content-center align-items-center flex-md-row justify-content-md-start w-100'}>
+                  <Image src={Icon1} alt={"icon 1"} width={120}
+                  height={90}></Image>
+                  <div className={'d-flex flex-column justify-content-center align-items-center'}>
+                      <h4 className={'p-0 m-0'}>亚洲半永久美容</h4>
+                      <h4 className={'p-0 m-0'}>大赛裁判长创办</h4>
+                  </div>
+
+              </div>
+
+                <div className={'d-flex flex-column justify-content-center align-items-center flex-md-row justify-content-md-start w-100'}>
+                    <Image src={Icon2} alt={"icon 1"} width={120}
+                           height={90}></Image>
+                    <div className={'d-flex flex-column justify-content-center align-items-center'}>
+                        <h4 className={'p-0 m-0'}>融合国际先进技术</h4>
+                    </div>
+
+                </div>
+
+                <div className={'d-flex flex-column justify-content-center align-items-center flex-md-row justify-content-md-start w-100'}>
+                    <Image src={Icon3} alt={"icon 1"} width={120}
+                           height={90}></Image>
+                    <div className={'d-flex flex-column justify-content-center align-items-center'}>
+                        <h4 className={'p-0 m-0'}>服务超过100000位客户</h4>
+                    </div>
+
+                </div>
+            </div>
+        </>
+
+    );
+}
+
+export default SecondPage;
