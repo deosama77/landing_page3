cae71b134a76935ec0c12ecc80b0b3f6ed2eb625
Osama Al Madhoun <o.almadhoun@unitone.co>
2022-12-07 09:11:26 +0200

    first page sample

--- components/LoadingPage.js
+++ components/LoadingPage.js
@@ -1,8 +1,14 @@
 import React from 'react';
-
+import styles from '../styles/Home.module.css'
+import {Spinner} from "react-bootstrap";
 function LoadingPage(props) {
     return (
-        <div>Loading ....</div>
+        <div className={styles.container+' '+styles.container_loader}>
+            <div className={'position-fixed '+styles.loader}>
+                <Spinner animation="border" variant="warning" className={styles.loader_spinner} />
+            </div>
+        </div>
+
     );
 }
 
