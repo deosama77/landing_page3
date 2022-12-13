9a43b8e31c9623992bfdb2d39a4284744529b232
Osama Al Madhoun <o.almadhoun@unitone.co>
2022-12-13 11:54:00 +0200

    updated responsive

--- components/FifthPage.js
+++ components/FifthPage.js
@@ -4,29 +4,25 @@ import ImageUp from '../public/images/page_5/wechat_image.png';
 import ImageBottom from "../public/images/page_5/wechat_bottom.png";
 import styles from '../styles/Fifth.module.css';
 import {Col,Row, Container} from "react-bootstrap";
-import ClickIcon from '../public/images/click_01_cut.png'
+import ClickIcon from '../public/images/page_5/icons-01_v5.png'
 function FifthPage(props) {
     return (
         <>
-        <div className={'position-absolute'}>
-            <Image src={ImageUp} alt={'picture 1'} width={300} height={600}
-            ></Image>
-        </div>
-        <div className={'position-absolute '+styles.image_bottom}>
-            <Image
-                   src={ImageBottom} alt={'picture 1'} width={400} height={500}></Image>
-        </div>
+            <div className={'position-absolute '+styles.left_image}>
+                <Image src={ImageUp} alt={'picture 1'} fill={+true}
+                ></Image>
+            </div>
 
             <Container className={'position-relative'}>
                 <Row className={'py-3 gap-2 d-flex justify-content-center w-100'}>
-                    <Col xs={12} md={5}  className={'py-4 px-4  bg-white bg-opacity-50 border rounded-5 border-warning ' +
-                        'd-flex flex-column align-items-center justify-content-md-center align-items-md-end'}>
+                    <Col xs={12} lg={5}  className={'py-4 px-4  bg-white bg-opacity-50 border rounded-5 border-warning ' +
+                        'd-flex flex-column justify-content-md-center align-items-end'}>
                         <div className={'h1 text-end '+styles.header_card}>3年严苛训练</div>
                         <div className={'h1 text-end '+styles.header_card}>造就精妙手艺</div>
                         <div className={'py-4 d-flex justify-content-center  align-items-center'}>
                             <div className={'mx-3'}>
-                                <div className={'h4 text-end'}>效果自然</div>
-                                <div className={'h4 text-end'}>灵动宛如天生</div>
+                                <div className={'h5 text-end'}>效果自然</div>
+                                <div className={'h5 text-end'}>灵动宛如天生</div>
                             </div>
                             <div className={styles.slash_number}>
                                 <span >1</span><div className={styles.slash}>/</div></div>
@@ -40,8 +36,8 @@ function FifthPage(props) {
                                 <span >2</span><div className={styles.slash}>/</div></div>
                         </div>
                     </Col>
-                    <Col xs={12} md={5}  className={'py-4 px-4  mt-2 mt-md-0  bg-white bg-opacity-50  border rounded-5 border-warning ' +
-                        'd-flex flex-column align-items-center justify-content-md-center align-items-md-start'}>
+                    <Col xs={12} lg={5}  className={'py-4 px-4  mt-2 mt-md-0  bg-white bg-opacity-50  border rounded-5 border-warning ' +
+                        'd-flex flex-column justify-content-md-center align-items-start'}>
                         <div className={'h1 text-start '+styles.header_card}>独家研发</div>
                         <div className={'h1 text-start '+styles.header_card}>智能对称<strong className={styles.app}>APP</strong></div>
 
@@ -49,9 +45,9 @@ function FifthPage(props) {
                             <div className={styles.slash_number}>
                                 <span >1</span><div className={styles.slash}>/</div></div>
                             <div className={'mx-3'}>
-                                <div className={'h4 text-start'}>测试个人脸型 、</div>
-                                <div className={'h4 text-start'}>眉骨、五官</div>
-                                <div className={'h4 text-start'}>量身定制眉形</div>
+                                <div className={'h5 text-start'}>测试个人脸型 、</div>
+                                <div className={'h5 text-start'}>眉骨、五官</div>
+                                <div className={'h5 text-start'}>量身定制眉形</div>
                             </div>
                         </div>
 
@@ -63,12 +59,16 @@ function FifthPage(props) {
                             </div>
 
                         </div>
+                        <div className={'position-absolute '+styles.image_bottom1}>
+                            <Image
+                                src={ImageBottom} alt={'picture 1'} fill={+true}></Image>
+                        </div>
                     </Col>
                 </Row>
-                <Row>
+                <Row className={'w-100 justify-content-center'}>
                     <div className={'d-flex align-items-start justify-content-center '+styles.click_icon}>
-                        <Image className={'fa-fade'}
-                               src={ClickIcon} alt={"click icon"} width={220} height={65}></Image>
+                        <Image className={'fa-fade'} style={{cursor:'pointer'}}
+                               src={ClickIcon} alt={"click icon"} fill={+true}></Image>
                     </div>
                 </Row>
             </Container>
