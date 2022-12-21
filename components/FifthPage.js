9563f937a2c47cadbde6c723403eee837373b0e0
Osama Al Madhoun <o.almadhoun@unitone.co>
2022-12-21 14:26:08 +0200

    fixes the sixeth page

--- components/FifthPage.js
+++ components/FifthPage.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Image from 'next/image';
-import ImageUp from '../public/images/page_5/wechat_image.png';
+import ImageUp from '../public/images/page_5/wechat_vertical.png';
 import ImageBottom from "../public/images/page_5/wechat_bottom.png";
 import styles from '../styles/Fifth.module.css';
 import {Col, Row, Container} from "react-bootstrap";
@@ -8,22 +8,22 @@ import ClickIcon from '../public/images/page_5/icons-01_v5.png'
 
 function FifthPage(props) {
     return (
-        <>
+        <div className={'px-3'}>
             <div className={'position-absolute '+styles.left_image}>
                 <Image src={ImageUp} alt={'picture 1'} fill={+true}
                 ></Image>
             </div>
 
             <Container className={'position-relative'}>
-                <Row className={'py-3 gap-2 d-flex justify-content-center w-100'}>
+                <Row className={'py-3 gap-2 d-flex justify-content-center w-100 m-0 p-0'}>
                     <Col xs={12} lg={5}  className={'py-4 px-4  bg-white bg-opacity-75 border rounded-5 border-warning ' +
                         'd-flex flex-column justify-content-md-center align-items-end'}>
-                        <div className={'h1 text-end '+styles.header_card}>3年严苛训练</div>
-                        <div className={'h1 text-end '+styles.header_card}>造就精妙手艺</div>
-                        <div className={'py-4 d-flex justify-content-center  align-items-center'}>
+                        <div className={'h1 text-end header_font '+styles.header_card}>3年严苛训练</div>
+                        <div className={'h1 text-end header_font '+styles.header_card}>造就精妙手艺</div>
+                        <div className={'py-5  d-flex justify-content-center  align-items-center'}>
                             <div className={'mx-3'}>
-                                <div className={'h5 text-end'}>效果自然</div>
-                                <div className={'h5 text-end'}>灵动宛如天生</div>
+                                <div className={'h5 text-end '+styles.second_text}>效果自然</div>
+                                <div className={'h5 text-end '+styles.second_text}>灵动宛如天生</div>
                             </div>
                             <div className={styles.slash_number}>
                                 <span >1</span><div className={styles.slash}>/</div></div>
@@ -39,16 +39,16 @@ function FifthPage(props) {
                     </Col>
                     <Col xs={12} lg={5}  className={'py-4 px-4  mt-2 mt-md-0  bg-white bg-opacity-50  border rounded-5 border-warning ' +
                         'd-flex flex-column justify-content-md-center align-items-start'}>
-                        <div className={'h1 text-start '+styles.header_card}>独家研发</div>
-                        <div className={'h1 text-start '+styles.header_card}>智能对称<strong className={styles.app}>APP</strong></div>
+                        <div className={'h1 text-start header_font '+styles.header_card}>独家研发</div>
+                        <div className={'h1 text-start header_font '+styles.header_card}>智能对称<strong className={styles.app}>APP</strong></div>
 
-                        <div className={'py-4 d-flex justify-content-center  align-items-center'}>
+                        <div className={'py-5 d-flex justify-content-center  align-items-center'}>
                             <div className={styles.slash_number}>
                                 <span >1</span><div className={styles.slash}>/</div></div>
                             <div className={'mx-3'}>
-                                <div className={'h5 text-start'}>测试个人脸型 、</div>
-                                <div className={'h5 text-start'}>眉骨、五官</div>
-                                <div className={'h5 text-start'}>量身定制眉形</div>
+                                <div className={'h5 text-start '+styles.second_text}>测试个人脸型 、</div>
+                                <div className={'h5 text-start '+styles.second_text}>眉骨、五官</div>
+                                <div className={'h5 text-start '+styles.second_text}>量身定制眉形</div>
                             </div>
                         </div>
 
@@ -66,14 +66,14 @@ function FifthPage(props) {
                         </div>
                     </Col>
                 </Row>
-                <Row className={'w-100 justify-content-center'}>
+                <Row className={'justify-content-center'}>
                         <div className={'d-flex align-items-start justify-content-center '+styles.click_icon}>
                             <Image className={'fa-fade'}
                                    src={ClickIcon} alt={"click icon"} fill={+true}></Image>
                         </div>
                 </Row>
             </Container>
-        </>
+        </div>
     );
 }
 
--- components/SixthPage1.js
+++ components/SixthPage1.js
@@ -12,21 +12,31 @@ function SixthPage1(props) {
     return (
         <Container>
             <Row className={'justify-content-center'}>
-                <Col xs={12} className={'text-center h1 '+styles.title}>Sense 捏骨式纹眉定妆</Col>
-                <Col xs={12} className={'text-center h1 '+styles.title}>实现素颜自由</Col>
+                <Col xs={12} className={'text-center header_font h1 '+styles.title}>Sense 捏骨式纹眉定妆</Col>
+                <Col xs={12} className={'text-center header_font h1 '+styles.title}>实现素颜自由</Col>
             </Row>
 
             <Row>
                 <Col xs={12} md={6} >
-                    <div className={"position-relative"} style={{height:350 , width:"100%"}}>
+                    <div className={"position-relative "+styles.h_left} style={{height:350 , width:"100%"}}>
                         <Image src={HLeft} alt={"left cut"}
+                               fill={+true}></Image>
+                    </div>
+
+                    <div className={"position-relative "+styles.v_top} style={{height:'40vh' , width:"100%"}}>
+                        <Image src={VTop} alt={"left cut"}
                                fill={+true}></Image>
                     </div>
                 </Col>
                 <Col xs={12} md={6} >
-                    <div className={"position-relative"} style={{height:350, width:"100%"}}>
+                    <div className={"position-relative "+styles.h_right} style={{height:350, width:"100%"}}>
                     <Image src={HRight} alt={"right cut"}
                            fill={+true}></Image>
+                    </div>
+
+                    <div className={"position-relative "+styles.v_bottom} style={{height:'40vh', width:"100%"}}>
+                        <Image src={VBottom} alt={"right cut"}
+                               fill={+true}></Image>
                     </div>
                 </Col>
             </Row>
